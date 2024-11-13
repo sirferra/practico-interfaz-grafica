97c1bb37673bb45680a173cd7ada2d6d8d734034
sirferra <sir.leandroferra@gmail.com>
2024-11-13 08:20:31 -0300

    fix: cambio string de conexión

--- src/main/java/negocio/mysql/ConnectionHolder.java
+++ src/main/java/negocio/mysql/ConnectionHolder.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConnectionHolder {
-    private static final String CONNECTION_STRING = "jdbc:mysql://root:1234@localhost:33006";
+    private static final String CONNECTION_STRING = "jdbc:mysql://root:1234@localhost:3306";
     private static final Connection connection;
     private static final SimpleORM orm;
     private ConnectionHolder() {
